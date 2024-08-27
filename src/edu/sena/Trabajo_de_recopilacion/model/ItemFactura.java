daa3ccb5c61d21d69295e06c7f86d8ecd0101407
andre <andres2345@gmail.com>
2024-08-27 15:16:27 -0500

    contructor de ItemFactura.java

--- src/edu/sena/Trabajo_de_recopilacion/model/ItemFactura.java
+++ src/edu/sena/Trabajo_de_recopilacion/model/ItemFactura.java
@@ -4,6 +4,13 @@ public class ItemFactura {
     private Producto producto;
     private int cantidad;
 
+    //CONSTRUCTOR
+    public ItemFactura(Producto producto, int cantidad){
+        this.producto = producto;
+        this.cantidad = cantidad;
+
+    }
+
     public Producto getProducto() {
         return producto;
     }
