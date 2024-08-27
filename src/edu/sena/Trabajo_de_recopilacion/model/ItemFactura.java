65d02804314e71df1dc3e7a1977544f4ea5e677c
andre <andres2345@gmail.com>
2024-08-27 14:26:12 -0500

    set y get de ItemFactura.java

--- src/edu/sena/Trabajo_de_recopilacion/model/ItemFactura.java
+++ src/edu/sena/Trabajo_de_recopilacion/model/ItemFactura.java
@@ -3,4 +3,20 @@ package edu.sena.Trabajo_de_recopilacion.model;
 public class ItemFactura {
     private Producto producto;
     private int cantidad;
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
 }
